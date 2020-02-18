8c41dfe2a72bd53e7cde8a9712a3c2b5ce8da351
Valentin Brosch <valentin.brosch@aibro.de>
2020-02-18 18:13:23 +0100

    some work on pnas

--- pnas/utilities.py
+++ pnas/utilities.py
@@ -1,10 +1,28 @@
 import copy
 import itertools
-from typing import List
+from typing import List, Dict
 
 from modules.block import Block
 from modules.cell import Cell
-from search_space import Operation
+from search_space import Operation, NUMBER_OF_BLOCKS_PER_CELL
+
+
+def _get_input_word(input_num: int) -> str:
+    """
+    convert this input number into the input word in the dictionary
+    :param input_num: the input num
+    :return: the input word
+    """
+    return 'input-'.format(input_num)
+
+
+def _get_op_word(op: Operation) -> str:
+    """
+    get the operation identifier in the dictionary
+    :param op: the operation
+    :return: the operation identifier
+    """
+    return 'op-{}'.format(op.__str__())
 
 
 def _get_vocabulary_size() -> int:
@@ -12,7 +30,18 @@ def _get_vocabulary_size() -> int:
     get the vocabulary size
     :return: the size of the vocabulary for the embeddings
     """
-    return 0
+    return len(_get_vocabulary())
+
+
+def _get_vocabulary() -> Dict[str, int]:
+    """
+    get the full vocabulary
+    :return: the vocabulary
+    """
+    vocabulary = [_get_input_word(i) for i in range(NUMBER_OF_BLOCKS_PER_CELL + 2)]
+    vocabulary += [_get_op_word(op) for op in list(Operation)]
+
+    return {w: i for i, w in enumerate(vocabulary)}
 
 
 def _get_all_possible_blocks(current_cell_size: int) -> List[Block]:
